2dedcaaa02a55e1c2c6deea5189074f453e10d4e
Isaias Jair de Oliveira Junior <jrissi2008@gmail.com>
2019-10-18 20:07:12 -0300

    Project Created

---
+++ src/main/java/com/jr/curso_mc/CourseMcApplication.java
@@ -0,0 +1,13 @@
+package com.jr.curso_mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jr/curso_mc/CourseMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jr.curso_mc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
