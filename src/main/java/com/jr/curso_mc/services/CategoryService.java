deceb68b5cf32bf881b522a6f2bd1cf613d9cc1e
Isaias Jair de Oliveira Junior <jrissi2008@gmail.com>
2019-10-20 07:37:40 -0300

    Adjusting category endpoint and creating exceptions

--- src/main/java/com/jr/curso_mc/services/CategoryService.java
+++ src/main/java/com/jr/curso_mc/services/CategoryService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jr.curso_mc.domain.Category;
 import com.jr.curso_mc.repositories.CategoryRepository;
+import com.jr.curso_mc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,6 +17,7 @@ public class CategoryService {
 	
 	public Category find(Integer id) {
 		Optional<Category> obj = repo.findById(id); 
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Category.class.getName()));
 	}
 }
